ae25afeda177a30543b64b7ccfd9795f101d497c
rachel <rachelliu356@gmail.com>
2026-05-15 16:35:29 -0400

    update mininet_testing.py

--- mininet_testing.py
+++ mininet_testing.py
@@ -1,23 +1,38 @@
 # For testing Mininet commands
 
+from mininet.net import Mininet
 from mininet.topo import Topo
+from mininet.node import OVSController
+from mininet.link import TCLink
+from mininet.cli import CLI
+from mininet.log import setLogLevel
 
-class MyTopo( Topo ):
+class TestTopo( Topo ):
     "Simple topology example."
 
     def build( self ):
         "Create custom topo."
 
         # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
 
         # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+        self.addLink(h1, s3)
+        self.addLink(s3, s4)
+        self.addLink(s4, h2)
 
+topos = { 'testtopo': ( lambda: TestTopo() ) }
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+def run():
+    setLogLevel('info')
+    topo = TestTopo()
+    net = Mininet(topo=topo, controller=OVSController, link=TCLink)
+    net.start()
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    run()
