7ef349ea5d65ab1dc3ed34552c9875b6c99bda5e
rachel <rachelliu356@gmail.com>
2026-05-18 16:38:38 -0400

    test array of strings

--- mininet_test_topo.py
+++ mininet_test_topo.py
@@ -10,8 +10,8 @@ class TestTopo(Topo):
         # Add switches
 
         s = [0]*3
-        for i in range(1, len(s)+1):
-            s[i-1] = self.addSwitch('s'+str(i))
+        for i in range(len(s)):
+            s[i] = self.addSwitch('s'+str(i))
         # s1 = self.addSwitch('s1')
         # s2 = self.addSwitch('s2')
         # s3 = self.addSwitch('s3')
@@ -25,16 +25,16 @@ class TestTopo(Topo):
         h6 = self.addHost('h6')
 
         # Edge/Substation links: hosts to switches
-        add_edge_link(self, h1, s1)
-        add_edge_link(self, h2, s1)
-        add_edge_link(self, h3, s2)
-        add_edge_link(self, h4, s2)
-        add_edge_link(self, h5, s3)
-        add_edge_link(self, h6, s3)
+        add_edge_link(self, h1, s[0])
+        add_edge_link(self, h2, s[0])
+        add_edge_link(self, h3, s[1])
+        add_edge_link(self, h4, s[1])
+        add_edge_link(self, h5, s[2])
+        add_edge_link(self, h6, s[2])
 
         # Core Backbone links: switches to switches
-        add_backbone_link(self, s1, s2)
-        add_backbone_link(self, s2, s3)
-        add_backbone_link(self, s3, s1)
+        add_backbone_link(self, s[0], s[1])
+        add_backbone_link(self, s[1], s[2])
+        add_backbone_link(self, s[2], s[0])
 
 topos = {'testtopo': (lambda: TestTopo())}
