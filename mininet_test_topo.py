88ff4eff23b10692460e316f34c8c39a8b9ec146
rachel <rachelliu356@gmail.com>
2026-05-18 16:41:19 -0400

    change hosts to list

--- mininet_test_topo.py
+++ mininet_test_topo.py
@@ -12,25 +12,19 @@ class TestTopo(Topo):
         s = [0]*3
         for i in range(len(s)):
             s[i] = self.addSwitch('s'+str(i))
-        # s1 = self.addSwitch('s1')
-        # s2 = self.addSwitch('s2')
-        # s3 = self.addSwitch('s3')
 
         # Add hosts
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        h5 = self.addHost('h5')
-        h6 = self.addHost('h6')
+        h = [0]*6
+        for i in range(len(h)):
+            h[i] = self.addHost('h'+str(i))
 
         # Edge/Substation links: hosts to switches
-        add_edge_link(self, h1, s[0])
-        add_edge_link(self, h2, s[0])
-        add_edge_link(self, h3, s[1])
-        add_edge_link(self, h4, s[1])
-        add_edge_link(self, h5, s[2])
-        add_edge_link(self, h6, s[2])
+        add_edge_link(self, h[0], s[0])
+        add_edge_link(self, h[1], s[0])
+        add_edge_link(self, h[2], s[1])
+        add_edge_link(self, h[3], s[1])
+        add_edge_link(self, h[4], s[2])
+        add_edge_link(self, h[5], s[2])
 
         # Core Backbone links: switches to switches
         add_backbone_link(self, s[0], s[1])
